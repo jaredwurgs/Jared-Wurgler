18b95e95aaf37b135d2c745a575fc9d0f1151591
jaredwurgs <42579255+jaredwurgs@users.noreply.github.com>
2018-08-23 14:28:14 -0500

    Added source file
    Entered my major.

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Jared Wurgler Taylor Kellog
+ * Date: 08/20/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc, char **argv) {
+
+  printf("Jared Wurgler Electrical Engineering!\n");
+
+  return 0;
+}
